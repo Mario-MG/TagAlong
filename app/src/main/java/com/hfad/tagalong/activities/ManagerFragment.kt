690cfcad73787020d13c6b58f6d14e6121294cc3
Mario MG <mariomg11k@gmail.com>
2021-01-31 21:16:35 +0100

    Implement spinner all/any for playlist creation

--- app/src/main/java/com/hfad/tagalong/activities/ManagerFragment.kt
+++ app/src/main/java/com/hfad/tagalong/activities/ManagerFragment.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.Toast
 import com.hfad.tagalong.R
 import com.hfad.tagalong.tools.DBHelper
 import com.hfad.tagalong.tools.api.PlaylistManager
 import com.hfad.tagalong.views.CustomTagManagerForPlaylistCreationView
+import kotlinx.android.synthetic.main.fragment_manager.*
 import kotlin.concurrent.thread
 
 class ManagerFragment : Fragment() {
@@ -43,8 +45,13 @@ class ManagerFragment : Fragment() {
             createPlaylistButton.setOnClickListener {
                 val selectedTags = tagManagerView.tagList
                 thread {
+                    val spinner = findViewById<Spinner>(R.id.create_playlist_spinner)
                     val dbHelper = DBHelper(activity)
-                    val songIds = dbHelper.selectSongIdsWithAllTags(*selectedTags.toTypedArray())
+                    val songIds = when (spinner.selectedItemPosition) {
+                        0 -> dbHelper.selectSongIdsWithAllTags(*selectedTags.toTypedArray())
+                        1 -> dbHelper.selectSongIdsWithAnyTags(*selectedTags.toTypedArray())
+                        else -> emptyList()
+                    }
                     val playlistCreatedResponse = PlaylistManager.createPlaylist("TagAlong Playlist")
                     if (songIds.isNotEmpty() && playlistCreatedResponse?.success() == true) {
                         val playlistId = playlistCreatedResponse.result?.id!!
